91d9eedb20b7e1d6febf6ad3757203b73f87d9ca
anjali <anjali001.as@gmail.com>
2023-01-18 13:15:37 +0530

    server changes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const compression = require('compression');
-const path = require('path');
-const URL = require('./models/Url');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const compression = require("compression");
+const path = require("path");
+const URL = require("./models/Url");
 //AuthRoutes
-const authRoute = require('./routes/auth');
-const dummy = require('./routes/dummy');
-
+const authRoute = require("./routes/auth");
+const dummy = require("./routes/dummy");
 
 //URL Routes
-const shorten = require('./routes/shorten');
+const shorten = require("./routes/shorten");
 
 const app = express();
 
@@ -23,54 +22,54 @@ app.use(cors());
 dotenv.config();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.use((req, res, next) => {
-      if (req.header("x-forwarded-proto") !== "https") {
-        res.redirect(`https://${req.header("host")}${req.url}`);
-      } else {
-        next();
-      }
-    });
-    app.use(express.static("client/build"));
+  app.use(express.static("build"));
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
+  app.use(express.static("client/build"));
 }
 
-mongoose.connect("mongodb+srv://anjaliproject:llamadatabase@cluster0.uzaoh.gcp.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=>console.log('Mongodb connected'))
-.catch(err=>console.log('Error:', err))
+mongoose
+  .connect(
+    "mongodb+srv://anjaliproject:llamadatabase@cluster0.uzaoh.gcp.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Mongodb connected"))
+  .catch((err) => console.log("Error:", err));
 //Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/user',authRoute);
-app.use('/api/dummy',dummy);
+app.use("/api/user", authRoute);
+app.use("/api/dummy", dummy);
 
 //URL work
-app.use('/api/shorten', shorten);
+app.use("/api/shorten", shorten);
 
 //redirect
-app.get('/:hash',(req,res)=>{
-    console.log(req.params)
-    const hashed = req.params.hash;
-    URL.findOne({hash:hashed},(err,doc)=>{
-        if(doc){
-            console.log(doc.url);
-            res.redirect('https://'+doc.url)
-        }else{
-            res.redirect('/')
-        }
-    })
-})
+app.get("/:hash", (req, res) => {
+  console.log(req.params);
+  const hashed = req.params.hash;
+  URL.findOne({ hash: hashed }, (err, doc) => {
+    if (doc) {
+      console.log(doc.url);
+      res.redirect("https://" + doc.url);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', ()=> () =>{
-        res.sendFile(
-            path.join(__dir,'client','build','index.html')
-        );
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", () => () => {
+    res.sendFile(path.join(__dir, "client", "build", "index.html"));
+  });
 }
 
-
-const Port = process.env.PORT || 5000
-app.listen(Port, ()=>console.log('Server up and running'));
-
+const Port = process.env.PORT || 5000;
+app.listen(Port, () => console.log("Server up and running"));
